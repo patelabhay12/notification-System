679deb9292eb388a17c47476c35c81ee2e70c1b0
Abhay Patel <abhaypatel6794@gmail.com>
2026-06-30 10:22:04 +0530

    initial commit

---
+++ Channel-Worker-Service/src/main/java/com/channel/worker/ChannelWorkerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.channel.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelWorkerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelWorkerServiceApplication.class, args);
+	}
+
+}
---
+++ Notification-Proccessor-Service/src/main/java/com/notification/processor/NotificationProccessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.notification.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationProccessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationProccessorServiceApplication.class, args);
+	}
+
+}
